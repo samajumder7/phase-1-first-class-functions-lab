108ce8ec6d2962fa88c10bf17bf8bb050f2fa731
Saswata Alex Majumder <majumder.ua@gmail.com>
2023-09-25 00:46:28 -0400

    FCF lab done

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers){
+    return drivers.slice(0,2);
+}
+function returnLastTwoDrivers(drivers){
+    return drivers.slice(-2);
+}
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(factor){
+    const multiplier = function (fare){ return fare * factor; }
+    return multiplier;
+}
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (drivers, selection){
+    return selection(drivers);
+}
